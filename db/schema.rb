dfb995d65838211fee1e639860d65a7e85564b6d
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-14 16:27:41 +0200

    created the tables model and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_14_135139) do
+ActiveRecord::Schema.define(version: 2021_06_14_142235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +21,14 @@ ActiveRecord::Schema.define(version: 2021_06_14_135139) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tables", force: :cascade do |t|
+    t.integer "number"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_tables_on_restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_135139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tables", "restaurants"
 end
