243ae2a59c2c4ecbb0062d01c93a04ce25255eed
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-14 18:05:07 +0200

    created the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Restaurant.destroy_all
+Table.destroy_all
+Basket.destroy_all
+Meal.destroy_all
+BasketMeal.destroy_all
+
+p "Creating User..."
+User.create!(first_name: "John", last_name: "Cena", email: "john_cena@gmail.com", password: "password")
+
+
+p "Creating Restaurant..."
+Restaurant.create!(name: "Blockhouse", address: "Blockhouse Street, 4, Hamburg", category: "Indian")
+
+p "Creating Tables..."
+Table.create!(number: 1, restaurant_id: Restaurant.first.id)
+Table.create!(number: 2, restaurant: Restaurant.first)
+Table.create!(number: 3, restaurant_id: Restaurant.first.id)
+
+p "Creating Baskets..."
+Basket.create!(user_id: User.first.id, table_id: Table.first.id)
+
+p "Creating Meals..."
+Meal.create!(name: "Steak", price: 5.80, description: "Large piece of meat...", restaurant_id: Restaurant.first.id)
+
+p "Creating BasketMeals..."
+BasketMeal.create!(basket_id: Basket.first.id, meal_id: Meal.first.id)
+
+p "Finished seeding"
