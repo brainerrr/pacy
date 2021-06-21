f4a664c38c364eefed5af5c30a7ce50897f1bdb8
Georg Liechtenstein <liechtensteingeorg@gmail.com>
2021-06-21 14:58:55 +0200

    fixing the js on the qr code

--- app/javascript/plugins/basic_qr_code_reader.js
+++ app/javascript/plugins/basic_qr_code_reader.js
@@ -1,16 +1,19 @@
 import { BrowserQRCodeReader } from '@zxing/library';
 const basicQrCodeReader = () => {
-  const codeReader = new BrowserQRCodeReader();
-console.log("hello world!!!")
-  codeReader
-    .decodeFromInputVideoDevice(undefined, 'video')
-    .then((result) => {
-      // process the result
-      console.log(result.text)
+  const qrCodeDiv = document.querySelector("#qr-code-reader")
+  if (qrCodeDiv) {
+    const codeReader = new BrowserQRCodeReader();
+    codeReader
+      .decodeFromInputVideoDevice(undefined, 'video')
+      .then((result) => {
+        // process the result
+        console.log(result.text)
 
-      document.getElementById('result').textContent = result.text
-    })
-    .catch(err => console.error(err));
+        document.getElementById('result').textContent = result.text
+        window.location.href = result.text
+      })
+      .catch(err => console.error(err));
+  }
 }
 
 
