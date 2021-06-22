259977d16e6853078b33e943a193ca34cccf7de3
Bogdan Dan <dan.f.bogdan@gmail.com>
2021-06-22 11:37:55 +0200

    add more items option implemented

--- app/controllers/basket_meals_controller.rb
+++ app/controllers/basket_meals_controller.rb
@@ -1,16 +1,15 @@
 class BasketMealsController < ApplicationController
   def create
-    @meal = Meal.find(params[:meal_id])
+    @meal = Meal.find(params[:basket_meal][:meal_id])
     @basket = Basket.find(params[:basket_id])
     if @basket.meals.include?(@meal)
       # find basket_meal where meal_id is @meal.id and basket_id is @basket.id
       # update quantity of basket meal
       @basket_meal = BasketMeal.find_by(meal_id: @meal.id, basket_id: @basket.id)
-      @basket_meal.quantity += 1
+      @basket_meal.quantity += params[:basket_meal][:quantity].to_i
       @basket_meal.save
     else
-      @basket_meal = BasketMeal.new
-      @basket_meal.meal = @meal
+      @basket_meal = BasketMeal.new(basket_meal_params)
       @basket_meal.basket = @basket
       @basket_meal.save
     end
@@ -33,6 +32,6 @@ class BasketMealsController < ApplicationController
   private
 
   def basket_meal_params
-    # params.require(:basket_meals).permit(:)
+    params.require(:basket_meal).permit(:meal_id, :quantity)
   end
 end
