6b663c38eb17070ad1edc3b7529e2f7ab5a7cf86
Maurice Welter <mauricewelter7@gmail.com>
2021-06-23 13:04:19 +0200

    stripe startimng to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_084332) do
+ActiveRecord::Schema.define(version: 2021_06_23_095721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,8 @@ ActiveRecord::Schema.define(version: 2021_06_23_084332) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status", default: 0, null: false
-    t.integer "amount_cents", default: 0, null: false
+    t.integer "total_price_cents", default: 0, null: false
+    t.string "checkout_session_id"
     t.index ["table_id"], name: "index_baskets_on_table_id"
     t.index ["user_id"], name: "index_baskets_on_user_id"
   end
