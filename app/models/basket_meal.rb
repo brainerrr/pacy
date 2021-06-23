85ed16a4c030c520b9b919337d76d848930d235c
Bogdan Dan <dan.f.bogdan@gmail.com>
2021-06-23 18:04:35 +0200

    validations of quantity negative

--- app/models/basket_meal.rb
+++ app/models/basket_meal.rb
@@ -3,11 +3,12 @@ class BasketMeal < ApplicationRecord
   belongs_to :meal
   has_many :users, through: :basket
   validate :quantity_is_greater
+  # validates :quantity, numericality: { greater_than: 0 }
   enum shared: { not_shared: 0, shared: 1 }
 
   def quantity_is_greater
-    if quantity <= 0
-      errors.add(:base, :invalid, message:"Quantity must be greater than 0")
+    unless quantity > 0
+      errors.add(:base, :invalid, message: "Quantity must be greater than 0")
     end
   end
 end
