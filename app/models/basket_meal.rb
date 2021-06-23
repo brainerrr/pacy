4c2d1df06cb377c14312656f819967a54fefe4a4
Bogdan Dan <dan.f.bogdan@gmail.com>
2021-06-23 11:46:40 +0200

    validation of quantity done

--- app/models/basket_meal.rb
+++ app/models/basket_meal.rb
@@ -2,4 +2,11 @@ class BasketMeal < ApplicationRecord
   belongs_to :basket
   belongs_to :meal
   has_many :users, through: :basket
+  validate :quantity_is_greater
+
+  def quantity_is_greater
+    if quantity.negative?
+      errors.add("Quantity must be greater than 0")
+    end
+  end
 end
